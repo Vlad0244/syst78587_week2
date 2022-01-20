08bc5d7d79f083db1d9ef7941e7e7b4f99d44cdc
suprun <suprun@sheridancollege.ca>
2022-01-19 22:30:49 -0500

    First Commit

---
+++ src/revstring/RevString.java
@@ -0,0 +1,26 @@
+package revstring;
+import java.util.Scanner;
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Volodymyr Suprun
+ */
+public class RevString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input a string");
+        String randomString = scan.nextLine();
+        char[] list1 = new char[randomString.length()];
+        for (int i = 0; i < randomString.length(); i++) {
+            list1[i] = randomString.charAt(i);
+        }
+        for (int i = randomString.length() - 1; i >= 0; i--) {
+            System.out.print(list1[i]);  
+        }
+    }
+
+}
